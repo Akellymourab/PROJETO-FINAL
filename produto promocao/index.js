50e2e03e88c72c13fcaca75650e84355ab194685
Akellymourab <101601082+Akellymourab@users.noreply.github.com>
2023-03-12 16:08:51 -0300

    produto promoçao e footer

---
+++ produto promocao/index.js
@@ -0,0 +1,20 @@
+// export default function Ppromocao(){
+    return (
+        <>
+            <div>
+            <img src="tenis.img.svg" alt="" />
+            </div>
+            <div>
+                <h4>OFERTA ESPECIAL </h4>
+                <hr/>
+                <h1>Air Jordan edição de colecionador</h1>
+                <hr/>
+                <p> É claro que o comprometimento entre as equipes cumpre um papel 
+                    essencial na formulação dos métodos utilizados na avaliação de resultados.</p>
+                <hr/> 
+                <button>VER OFERTA</button>    
+            </div>
+        </>
+    );
+// }
+
