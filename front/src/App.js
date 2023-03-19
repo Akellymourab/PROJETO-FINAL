7a9b592a6bc3e405a41a8a04bea9fb65376bd258
Marilia Mota <marilia.mota90@gmail.com>
2023-03-19 20:50:42 -0300

    backup final

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,19 @@
 import Produto from "./components/Produto";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "../src/components/Produto/style.css"
 import "./estilo.css"
+import Produtos from "./pages/Produtos";
 
 export default function App()
 {
   return(
     <div>
-      <Produto/>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/produtos" element={<Produtos/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
